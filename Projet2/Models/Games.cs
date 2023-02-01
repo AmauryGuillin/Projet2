8dccf1feb6781d2e5ac30b956b1f99e92ee9ebd0
Abigael <abigael.castillo@live.fr>
2023-02-01 13:01:24 +0100

    edit C.Games

--- Projet2/Models/Games.cs
+++ Projet2/Models/Games.cs
@@ -1,11 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Projet2.Models
 {
+    public enum GameList
+    {
+        Mk,
+        Lol,
+        Stf
+    }
+
     public class Games
     {
 		public int Id { get; set; }
         public string GameType { get; set; }
-        public virtual List<Games> GameList { get; set; } // enum a faire
+        public GameList GameList { get; set; }
     }
 }
