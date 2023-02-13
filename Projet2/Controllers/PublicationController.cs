ccae6683012f4c9d3e6fd610478098f3423d6e73
Abigael <abigael.castillo@live.fr>
2023-02-13 16:31:30 +0100

    OnPublication OK

--- Projet2/Controllers/PublicationController.cs
+++ Projet2/Controllers/PublicationController.cs
@@ -62,11 +62,13 @@ namespace Projet2.Controllers
             string uploads = Path.Combine(_webEnv.WebRootPath, "images");
             string filePath = Path.Combine(uploads, model.Publication.PubliImage.FileName);
             using (Stream fileStream = new FileStream(filePath, FileMode.Create))
-
-                if (HttpContext.User.Identity.IsAuthenticated == true)
+            {
+                model.Publication.PubliImage.CopyTo(fileStream);
+            }
+            if (HttpContext.User.Identity.IsAuthenticated == true)
             {
                 string accountId = (HttpContext.User.Identity.Name);
-                model.Account = dal.GetAccount(accountId);
+                model.Account = dal.GetAccount(accountId);   
                 Account userAccount = model.Account;
                 Publication publi = dal.CreatePublication(
                 "/images/" + model.Publication.PubliImage.FileName,
@@ -94,6 +96,9 @@ namespace Projet2.Controllers
             {
                 model.Publication = dal.GetOnePublication(id);
                 Publication publication = model.Publication;
+
+                model.Publication.Account = dal.GetAccounts().Where(r => r.Id == publication.AccountId).FirstOrDefault();
+                
                 return View(model);
             }
             return RedirectToAction("Login", "Login");
